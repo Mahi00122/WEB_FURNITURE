3bbcfa2b0092f6f688d19952a76950f177fe4f21
Mahi <mahi.gupta.mdg2006@gmail.com>
2026-03-13 10:54:32 +0530

    sdffd

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os, uuid, io
 app = Flask(__name__)
 CORS(app)  # allow all origins
 # url for ngrok
+# here we  are activating jenkins
 
 
 # ------------------------------------------------------------------ #
